bd1f970b7c3a5d6e5cce15b76ba3b5d1d3a0ec02
Shafaa Munjal <shafaam@wharton.upenn.edu>
2024-05-03 20:52:20 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import {Route, Routes} from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
+import LoginPage from './pages/LoginPage/LoginPage'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Routes>
+        <Route path = '/' element = {<HomePage />}/>
+        <Route path = '/login' element = {<LoginPage />}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/AuthForm/AuthForm.jsx
@@ -0,0 +1,38 @@
+import { Box, VStack, Input, Image, Button, Text, HStack} from '@chakra-ui/react'
+import {useState} from 'react';
+
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  return (
+  <>
+    <Box border = {"1px solid gray"} borderRadius={4} padding={5}>
+      <VStack spacing={4}>
+        <Image src = '/logo.png' h={24} cursor={"pointer"}alt = 'Instagram'/>
+        <Input placeholder='Email' fontSize={14} type='email'
+        value={inputs.email}
+        onChange={(e) => setInputs({...inputs, email: e.target.value})}
+        />
+        <Input placeholder='Password' fontSize={14}
+          type='password' value={inputs.password} 
+          onChange={(e) => setInputs({...inputs, email: e.target.value})}/>
+
+    <HStack spacing='24px'>
+      <Button colorScheme='blue'>Log In</Button>
+      <Button colorScheme='blue'>Sign Up</Button> 
+    </HStack>
+    
+
+      </VStack>
+    </Box>
+  </>
+  );
+}
+
+export default AuthForm;
